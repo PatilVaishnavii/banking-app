4984e328ab6e60917d8bfda4877b561dd13f2a89
PatilVaishnavii <vaishnavip488@gmail.com>
2024-05-23 14:44:18 +0530

    update layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,16 +8,6 @@ export default function RootLayout({
     return (
       <main className="flex min-h-screen w-full justify-between font-inter">
         {children}
-        <div className="auth-asset">
-          <div>
-            <Image 
-            src="/icons/auth-image.svg"
-            alt="Auth image"
-            width={500}
-            height={500} 
-            />
-          </div>
-        </div>
       </main>
     );
   }
